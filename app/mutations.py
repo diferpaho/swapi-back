a881ed087f8edd5595893f8317a33a6ab3b362bb
Diego Palacios <diferpaho@gmail.com>
2022-02-02 20:45:04 -0500

    added mutation for people and and the name of the mutation planet was changed

--- app/mutations.py
+++ app/mutations.py
@@ -1,12 +1,12 @@
 import graphene
 from graphql_relay import from_global_id
 
-from .models import Planet
-from .types import PlanetType
+from .models import Planet, People
+from .types import PlanetType,PeopleType
 from .utils import generic_model_mutation_process
 
 
-class AddPlanetMutation(graphene.relay.ClientIDMutation):
+class PostPlanetMutation(graphene.relay.ClientIDMutation):
     class Input:
         id = graphene.ID(required=False)
         name = graphene.String(required=True)
@@ -30,4 +30,30 @@ class AddPlanetMutation(graphene.relay.ClientIDMutation):
             data['id'] = from_global_id(raw_id)[1]
 
         planet = generic_model_mutation_process(**data)
-        return AddPlanetMutation(planet=planet)
+        return PostPlanetMutation(planet=planet)
+
+class PostPeopleMutation(graphene.relay.ClientIDMutation):
+    class Input:
+        id = graphene.ID(required=False)
+        name = graphene.String(required=True)
+        mass = graphene.String(required=True)
+        height = graphene.String(required=True)
+        hair_color = graphene.String(required=True)
+        skin_color = graphene.String(required=True)
+        eye_color = graphene.String(required=True)
+        birth_year = graphene.String(required=True)
+        gender = graphene.String(required=False)
+        home_world = graphene.GlobalID(parent_type=PlanetType, required=True)
+
+    people = graphene.Field(PeopleType)
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **input):
+        raw_id = input.get('id', None)
+
+        data = {'model': People, 'data': input}
+        if raw_id:
+            data['id'] = from_global_id(raw_id)[1]
+
+        people = generic_model_mutation_process(**data)
+        return PostPeopleMutation(people=people)
